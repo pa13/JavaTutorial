cd391a3babe02b41cec8a558fc140da4f150ef1e
pattila <pappattis13@gmail.com>
2017-11-13 16:00:07 +0100

    init

---
+++ src/TutorialPoint/App.java
@@ -0,0 +1,25 @@
+package TutorialPoint;
+
+public class App {
+    public static void main(String[] args) {
+        Puppy myPuppy = new Puppy("tommy");
+        myPuppy.setAge(2);
+        myPuppy.getAge();
+        System.out.println("Variable Value :" + myPuppy.puppyAge);
+
+        /* Create two objects using constructor */
+        Employee empOne = new Employee("James Smith");
+        Employee empTwo = new Employee("Mary Anne");
+
+        // Invoking methods for each object created
+        empOne.empAge(26);
+        empOne.empDesignation("Senior Software Engineer");
+        empOne.empSalary(1000);
+        empOne.printEmployee();
+
+        empTwo.empAge(21);
+        empTwo.empDesignation("Software Engineer");
+        empTwo.empSalary(500);
+        empTwo.printEmployee();
+    }
+}
---
+++ src/TutorialPoint/FormFő.java
@@ -0,0 +1,4 @@
+package TutorialPoint;
+
+import javax.swing.*;
+
